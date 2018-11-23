7ad614c476822c2ebfe67e9909877cffd9235682
geogeocutcut <lefevregeoffrey@gmail.com>
2018-11-24 00:48:50 +0100

    refactoring repository

--- libragri.core.repository/Repository.cs
+++ libragri.core.repository/Repository.cs
@@ -7,37 +7,37 @@ namespace libragri.core.repository
 {
     public class Repository<TId, TEntity> : IRepository<TId, TEntity> where TEntity : Entity<TId>
     {
-        IStore<TId> store;
+        private IStore<TId> _store;
 
-        public Repository(IUnitOfWork<TId> uow)
+        public Repository(IStore<TId> store)
         {
-            this.store=uow.GetStore();
+            _store=store;
         }
 
         public async Task DeleteAsync(TEntity entity)
         {
-            await store.RemoveAsync<TEntity>(entity);
+            await _store.RemoveAsync<TEntity>(entity);
         }
 
         public async Task<IList<TEntity>> GetAllAsync()
         {
-            return await store.GetAllAsync<TEntity>();
+            return await _store.GetAllAsync<TEntity>();
         }
 
         public async Task<TEntity> GetByIdAsync(TId id)
         {
-            return await store.GetByIdAsync<TEntity>(id);
+            return await _store.GetByIdAsync<TEntity>(id);
         }
 
         
         public async Task<IList<TEntity>> FindAsync(System.Linq.Expressions.Expression<System.Func<TEntity, bool>> predicate)
         {
-            return await store.FindAsync<TEntity>(predicate);
+            return await _store.FindAsync<TEntity>(predicate);
         }
 
         public async Task<TEntity> UpsertAsync(TEntity entity)
         {
-            await store.UpsertAsync<TEntity>(entity);
+            await _store.UpsertAsync<TEntity>(entity);
             return entity;
         }
         
